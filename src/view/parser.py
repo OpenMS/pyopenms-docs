64a68b164b106573c13f4d981a5c145cb5221465
Fabian Rosner <fabian@rosner.email>
2020-06-03 16:57:55 +0200

    optimized parser.py

--- src/view/parser.py
+++ src/view/parser.py
@@ -6,12 +6,8 @@ def parser(file):
         for line in inp:
             if line.startswith("PRH"):
                 PRT.append(line.strip().split('\t'))
-            elif line.startswith("PRT"):
+            elif line.startswith("PRT") and not line.endswith("protein_details\n"):
                 PRT.append(line.strip().split('\t'))
             elif line.startswith("PSH") or line.startswith("PSM"):
                 PSM.append(line.strip().split('\t'))
 
-    l = len(PRT[0]) - 1
-    for i in PRT:
-        if i[l] == 'protein_details':
-            PRT.remove(i)
