4caf9509a8f0b5c3c95a05daf9b804aeeb1836e6
Fabian Rosner <fabian@rosner.email>
2020-06-03 15:13:02 +0200

    Changes on parser.py

--- src/view/parser.py
+++ src/view/parser.py
@@ -1,20 +1,15 @@
 def parser(file):
     PRT = []
     PSM = []
-    counter1 = 0
-    counter2 = 0
 
     with open(file) as inp:
         for line in inp:
             if line.startswith("PRH"):
                 PRT.append(line.strip().split('\t'))
-                counter1 + 1
             elif line.startswith("PRT"):
                 PRT.append(line.strip().split('\t'))
-                counter1 + 1
             elif line.startswith("PSH") or line.startswith("PSM"):
                 PSM.append(line.strip().split('\t'))
-                counter2 + 1
 
     l = len(PRT[0]) - 1
     for i in PRT:
