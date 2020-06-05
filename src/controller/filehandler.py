0a59347ddc4eee2059a92930e7f67e6b2a218273
Till Englert <till.englert@student.uni-tuebingen.de>
2020-06-05 19:30:42 +0200

    Changed Header and output of createrawtable
    Changed rawtable function table header and output

--- src/controller/filehandler.py
+++ src/controller/filehandler.py
@@ -25,34 +25,33 @@ def tagfiles(files, delimiter):
 
 def createRawTable(tagdict, inputdir):
     columnregex = ["TR[0-9]+", "F[0-9]+", "S[0-9]+"]
-    header = ['Fraction_Group', 'Fraction',
-              'Spectra_Filepath', 'Label', 'Sample']
+    header = ['Group', 'Fraction',
+              'Filepath', 'Label', 'Sample']
     index = []
     rows = []
     filenames = tagdict.keys()
     for file in filenames:
         index.append(file)
-        filtered_tags = {'Fraction_Group': 0, 'Fraction': 0,
-                         'Spectra_Filepath': str(inputdir+file),
+        filtered_tags = {'Group': 0, 'Fraction': 0,
+                         'Filepath': str(inputdir+file),
                          'Label': 0, 'Sample': 0}
         filetags = tagdict[file]
         for tag in filetags:
             if re.match(columnregex[0], tag):
-                filtered_tags['Fraction_Group'] = tag.split('TR')[1]
+                filtered_tags['Group'] = tag.split('TR')[1]
             elif re.match(columnregex[1], tag):
                 filtered_tags['Fraction'] = tag.split('F')[1]
             elif re.match(columnregex[2], tag):
                 filtered_tags['Sample'] = tag.split('S')[1]
         rows.append([filtered_tags[i]for i in header])
     rawtable = pd.DataFrame(rows, index=index, columns=header)
-    return rawtable
+    return(rawtable, header, index)
 
 
 # just for testing
 i = input("filepath: ")
-
 delimiters = ["_"]
 files = filehandler(i)
 preparedfiles = tagfiles(files, delimiters[0])
-rawtable = createRawTable(preparedfiles, i)
+rawtable, header, index = createRawTable(preparedfiles, i)
 print(rawtable)
