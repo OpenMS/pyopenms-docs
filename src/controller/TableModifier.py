0f5f33337190cff635c60cac97de281ff0c5a884
Till Englert <till.englert@student.uni-tuebingen.de>
2020-06-06 02:16:15 +0200

    Added functions to modify the dataframe
    Added functions to modify the dataframe, also for saving the dataframe and a searchfunction

---
+++ src/controller/TableModifier.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+
+class TableModifier:
+    def __init__(self, dataframe):
+        self.dataframe = dataframe
+
+    def save(self, dataframe, outputpath):
+        self.dataframe.to_csv(outputpath, sep='\t', encoding='utf-8')
+
+    def modifyGroup(self, rows, groupnum):
+        for row in rows:
+            self.dataframe.at[row, 'Fraction_Group'] = groupnum
+
+        return self.dataframe
+
+    def modifyFraction(self, rows, fractionnum):
+        for row in rows:
+            self.dataframe.at[row, 'Fraction'] = fractionnum
+
+        return self.dataframe
+
+    def modifySample(self, rows, samplenum):
+        for row in rows:
+            self.dataframe.at[row, 'Sample'] = samplenum
+
+        return self.dataframe
+
+    def modifyLabel(self, rows, labelnum):
+        for row in rows:
+            self.dataframe.at[row, 'Label'] = labelnum
+
+        return self.dataframe
+
+    def searchTable(self, dataframe, searchstring):
+        header = list(dataframe.columns.values)
+        indexesfound = []
+        for col in header:
+            indexesfound.append(dataframe[col].str.find(searchstring))
+
+        return indexesfound
+    
