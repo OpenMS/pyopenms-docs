42a71fe163e86381081b404bb4f6cacd1163d98e
Till Englert <till.englert@student.uni-tuebingen.de>
2020-06-09 11:14:28 +0200

    Added docstring and a modify Fraction
    Modify fraction can either take one number, or a range of numbers

--- src/controller/TableModifier.py
+++ src/controller/TableModifier.py
@@ -6,30 +6,67 @@ class TableModifier:
         self.dataframe = dataframe
 
     def modifyGroup(self, rows, groupnum):
+        """
+        Let the user set the group for a list of selected rows.
+        returns the modified dataframe
+        """
         for row in rows:
             self.dataframe.at[row, 'Fraction_Group'] = groupnum
 
         return self.dataframe
 
     def modifyFraction(self, rows, fractionnum):
+        """
+        Let the user set the fraction for a list of selected rows.
+        enabled, when only parsing one number as fraction
+        returns the modified dataframe
+        """
         for row in rows:
             self.dataframe.at[row, 'Fraction'] = fractionnum
 
         return self.dataframe
+    
+    def modifyFraction(self, rows, fractionnummin, fractionnummax):
+        """
+        Let the user set the fraction for a list of selected rows.
+        enabled, when parsing a range of numbers as fractions
+        returns the modified dataframe
+        """
+        if rows.lenght() < (fractionnummax-fractionnummin):
+            return -1
+        
+        else: 
+            for i in range(rows.lenght()):
+                fractionnum = fractionnummin + i
+                self.dataframe.at[row, 'Fraction'] = fractionnum
+
+            return self.dataframe
 
     def modifySample(self, rows, samplenum):
+        """
+        Let the user set the sample for a list of selected rows.
+        returns the modified dataframe
+        """
         for row in rows:
             self.dataframe.at[row, 'Sample'] = samplenum
 
         return self.dataframe
 
     def modifyLabel(self, rows, labelnum):
+        """
+        Let the user set the label for a list of selected rows.
+        returns the modified dataframe
+        """
         for row in rows:
             self.dataframe.at[row, 'Label'] = labelnum
 
         return self.dataframe
 
     def searchTable(self, dataframe, searchstring):
+        """
+        Let the user search in all cells of the dataframe.
+        returns the a 2d list which corresponds to the cells, contains -1 if nothing is found the indeces of the searchstring if found.
+        """
         header = list(dataframe.columns.values)
         indexesfound = []
         for col in header:
