224d0634d253dff71eb846fe257a0da5c1591acc
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2021-10-29 15:30:11 +0200

    add jupyter config

---
+++ jupyter_notebook_config.py
@@ -0,0 +1,16 @@
+
+   
+# Traitlet configuration file for jupyter-notebook.
+
+c.ServerProxy.servers = {
+    'openrefine': {
+        'command': ['python3', '-m', 'bokeh', 'serve', 'bokehfolderapp', '-p', '{port}','--allow-websocket-origin=*'],
+        'port': 3333,
+        'timeout': 120,
+        #'launcher_entry': {
+        #    'enabled': True,
+        #    'icon_path': '/home/jovyan/.jupyter/open-refine-logo.svg',
+        #    'title': 'OpenRefine',
+        #},
+    },
+}
