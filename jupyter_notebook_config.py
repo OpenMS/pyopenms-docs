a354305ef8d10ebaf9b11a394ef7c1b305411e6b
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2021-10-29 16:47:15 +0200

    try fix?

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -4,12 +4,12 @@
 
 c.ServerProxy.servers = {
     'massspecviewer': {
-        'command': ['python3', '-m', 'bokeh', 'serve', 'bokehappfolder', '--port', '{port}','--allow-websocket-origin=*'],
+        'command': ['python3', '-m', 'bokeh', 'serve', 'bokehappfolder', '--address', '0.0.0.0', '--port', '{port}', '--prefix', '{base_url}/proxy/{port}', '--allow-websocket-origin=*', '--use-xheaders'],
         'port': 3333,
         'timeout': 120,
         'launcher_entry': {
             'enabled': True,
-            'icon_path': '/home/jovyan/.jupyter/OpenMS.svg',
+            'icon_path': '/home/jovyan/OpenMS.svg',
             'title': 'pyOpenMS MS Viewer',
         },
     },
