18ee706963d015c41bcc4489f6360f2046364ad7
Julianus Pfeuffer <jpfeuffer@fu-berlin.de>
2021-10-29 15:07:52 +0200

    reqs

--- .binder/bokehserverextension.py
+++ .binder/bokehserverextension.py
@@ -1,5 +1,5 @@
 from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
-"""serve the bokeh-app directory with bokeh server"""
-Popen(["bokeh", "serve", "bokehappfolder", "--allow-websocket-origin=*"])
+	"""serve the bokeh-app directory with bokeh server"""
+	Popen(["bokeh", "serve", "bokehappfolder", "--allow-websocket-origin=*"])
