13287417a12f39d8600c240dadf3a1a64de096e9
Julianus Pfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2023-04-17 14:20:30 +0200

    exclude templates

--- docs/source/generate_ipynb.sh
+++ docs/source/generate_ipynb.sh
@@ -4,9 +4,11 @@ echo "Process Started"
 
 shopt -s globstar
 for FILE in **/*.rst; do
-  a=(${FILE/./ })
-  pandoc ${FILE} -o ${a[0]}.ipynb --filter ../pandoc_filters/admonitionfilter.py --filter ../pandoc_filters/code_pandocfilter.py --filter ../pandoc_filters/ignore_pandocfilter.py --filter ../pandoc_filters/transformlinks_pandocfilter.py
-  echo "${a[0]}.ipynb Generated"
+  if [[ $string != *"_templates"* ]]; then
+    a=(${FILE/./ })
+    pandoc ${FILE} -o ${a[0]}.ipynb --filter ../pandoc_filters/admonitionfilter.py --filter ../pandoc_filters/code_pandocfilter.py --filter ../pandoc_filters/ignore_pandocfilter.py --filter ../pandoc_filters/transformlinks_pandocfilter.py
+    echo "${a[0]}.ipynb Generated"
+  fi
 done
 
 echo "Process Completed Successfully"
