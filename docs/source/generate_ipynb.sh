a583c73b0dad03f7e651a0bea92a0f140c9006fe
Julianus Pfeuffer <pfeuffer@informatik.uni-tuebingen.de>
2023-04-24 21:32:12 +0200

    Try pandoc 3.x for generating ipynbs (#381)
    * Try pandoc 3.x for generating ipynbs
    
    * also update test-pr.yaml
    
    * generate notebooks recursively for new structure
    
    * exclude templates

--- docs/source/generate_ipynb.sh
+++ docs/source/generate_ipynb.sh
@@ -2,6 +2,13 @@
 
 echo "Process Started"
 
-for FILE in *.rst; do a=(${FILE/./ }); pandoc ${FILE} -o ${a[0]}.ipynb --filter ../pandoc_filters/admonitionfilter.py --filter ../pandoc_filters/code_pandocfilter.py --filter ../pandoc_filters/ignore_pandocfilter.py --filter ../pandoc_filters/transformlinks_pandocfilter.py; echo "${a[0]}.ipynb Generated"; done
+shopt -s globstar
+for FILE in **/*.rst; do
+  if [[ $string != *"_templates"* ]]; then
+    a=(${FILE/./ })
+    pandoc ${FILE} -o ${a[0]}.ipynb --filter ../pandoc_filters/admonitionfilter.py --filter ../pandoc_filters/code_pandocfilter.py --filter ../pandoc_filters/ignore_pandocfilter.py --filter ../pandoc_filters/transformlinks_pandocfilter.py
+    echo "${a[0]}.ipynb Generated"
+  fi
+done
 
 echo "Process Completed Successfully"
