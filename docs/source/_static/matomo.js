fd72727c56d512cbc816db64fad39c9095211ad5
Tjeerd Dijkstra <tjeerd.dijkstra@uni-tuebingen.de>
2026-01-29 23:29:04 +0100

    Revise Matomo tracking code for new endpoint
    Updated Matomo Analytics tracking code to use new URL and site ID.

--- docs/source/_static/matomo.js
+++ docs/source/_static/matomo.js
@@ -1,12 +1,13 @@
-// Matomo Analytics tracking code
-var _paq = window._paq = window._paq || [];
-/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
-(function() {
-  var u="https://openms-web.containers.piwik.pro/";
-  _paq.push(['setTrackerUrl', u+'matomo.php']);
-  _paq.push(['setSiteId', '977a289b-8290-4244-920c-847eb3da6b09']);
-  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-})();
+<script>
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="https://openms.matomo.cloud/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '3']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src='https://cdn.matomo.cloud/openms.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
